a3fb641b8fc7d8e3651804eac725a50747ced139
Craig McLure <craig@mclure.net>
2025-09-17 20:50:13 +0100

    Slight shuffling, added flatpakrepo

--- repo/scripts/install-flatpak.sh
+++ repo/scripts/install-flatpak.sh
@@ -4,7 +4,7 @@ set -e
 USER="frostycoolslug"
 REPOSITORY="repo-test"
 
-FLATPAK_REF="https://${USER}.github.io/${REPOSITORY}/beacn-utility.flatpakref"
+FLATPAK_REF="https://${USER}.github.io/${REPOSITORY}/flatpak/beacn-utility.flatpakref"
 
 echo "Installing Beacn Utility via Flatpak"
 flatpak install ${FLATPAK_REF}
